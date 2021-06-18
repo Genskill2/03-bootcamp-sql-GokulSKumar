6460963f94cad9124f6b86f0fe5271de8afbc1ee
GokulSKumar <84574607+GokulSKumar@users.noreply.github.com>
2021-06-18 10:36:05 +0530

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -1,1 +1,1 @@
-select book.title, publisher.name from books inner join publisher on publisher.id= books.publisher where publisher in (select publisher.id from publisher where country="UK");
+select books.title, publisher.name from books inner join publisher on publisher.id= books.publisher where publisher in (select publisher.id from publisher where country="UK");
